511316e517c6ce05e2d796a57ea751c93d89f276
Ubuntu <ubuntu@ip-172-31-12-18.us-west-2.compute.internal>
2013-07-23 22:54:08 +0000

    url w gradrze

--- grader.js
+++ grader.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#!/Usr/Bin/env node
 /*
 Automatically grade files for the presence of specified HTML tags/attributes.
 Uses commander.js and cheerio. Teaches command line application development
@@ -24,8 +24,22 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+
+var gradeUrl = function(url, programChecks) {
+    rest.get(url).on('complete', function(result) {
+        if (result instanceof Error) {
+            console.log('Error:affd ' + result.message);
+            process.exit(1);
+        } else {
+            var checkJson = checkHtmlFile(result, programChecks);
+            var outJson = JSON.stringify(checkJson, null, 4);
+            console.log(outJson);            
+        }
+    });
+}
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -37,7 +51,15 @@ var assertFileExists = function(infile) {
 };
 
 var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+    if (fs.existsSync(htmlfile))
+        return cheerio.load(fs.readFileSync(htmlfile));
+    else 
+        return cheerio.load(htmlfile);
+        
+};
+
+var cheerioHtmlFileFromUrl = function(urlResult) {
+    return cheerio.load(urlResult);
 };
 
 var loadChecks = function(checksfile) {
@@ -46,6 +68,7 @@ var loadChecks = function(checksfile) {
 
 var checkHtmlFile = function(htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
+console.log($);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -64,12 +87,32 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists))
+        .option('-u, --url <url>', 'Url to file', clone(function(url){return(url)}))
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+console.log(program.url);
+    if (program.file) {
+        var checkJson = checkHtmlFile(program.file, program.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+    } else {
+        gradeUrl(program.url, program.checks);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
 
+
+/*
+var rest = require('restler');
+var rest = require('restler');
+var rest = require('restler');
+rest.get('http://google.com').on('complete', function(result) {
+  if (result instanceof Error) {
+    console.log('Error: ' + result.message);
+    this.retry(5000); // try again after 5 sec
+  } else {
+    console.log(result);
+  }
+});
+*/
